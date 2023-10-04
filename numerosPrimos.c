71ee9303ca42cb4451cab6d8e26f9f553d8babff
Emily Asimbaya <a_asimbaya@hotmail.com>
2023-10-04 17:11:29 -0500

    [last]

--- numerosPrimos.c
+++ numerosPrimos.c
@@ -11,4 +11,23 @@ int main()
 
     int prim;
     bool noPrim;
-}
+
+
+  if (num <= 1) {
+        printf("\nNo es primo\n");
+    } else {
+        for (int i = 2; i <= num / 2; i++) {
+            prim = num % i;
+            if (prim == 0) {
+                noPrim = 0;
+            } else {
+                noPrim = 1;
+            }
+        }
+    }
+if (noPrim == 0) {
+            printf("No es primo");
+        } else {
+            printf("Es primo");
+        }
+    }
